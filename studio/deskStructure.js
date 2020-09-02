53fc4ae60d32f511cd50cdafe0b57e1fa148b237
majaalin <maja_alin@hotmail.com>
2020-09-02 15:01:54 +0200

    Change icons on list item

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,4 +1,7 @@
 import S from '@sanity/desk-tool/structure-builder';
+import MdHome from 'react-icons/lib/md/home';
+import MdEvent from 'react-icons/lib/md/event';
+import MdPlace from 'react-icons/lib/md/place';
 
 export default () =>
   S.list()
@@ -6,11 +9,14 @@ export default () =>
     .items([
       S.listItem()
         .title('Startsida')
-        .child(S.editor().schemaType('main').documentId('main')),
+        .child(S.editor().schemaType('main').documentId('main'))
+        .icon(MdHome),
       S.listItem()
         .title('Kontaktuppgifter')
-        .child(S.editor().schemaType('contact').documentId('contact')),
+        .child(S.editor().schemaType('contact').documentId('contact'))
+        .icon(MdPlace),
       S.listItem()
         .title('Event')
-        .child(S.editor().schemaType('event').documentId('event')),
+        .child(S.editor().schemaType('event').documentId('event'))
+        .icon(MdEvent),
     ]);
