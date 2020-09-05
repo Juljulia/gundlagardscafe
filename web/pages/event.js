f7cc8822fe55086a4cc72d7b3c20d29474e2bc65
majaalin <maja_alin@hotmail.com>
2020-09-05 16:49:11 +0200

    Update schema food

---
+++ web/pages/event.js
@@ -0,0 +1,22 @@
+import '../styles/sass/style.scss';
+import groq from 'groq';
+import client from '../client';
+
+const Event = (props) => {
+  const { header = '' } = props;
+  return (
+    <div>
+      <h1>{header}</h1>
+    </div>
+  );
+};
+
+const query = groq`*[_type == 'event'][0]{
+    header,
+  }`;
+
+Event.getInitialProps = async function () {
+  return await client.fetch(query);
+};
+
+export default Event;
---
+++ web/pages/food.js
@@ -0,0 +1,27 @@
+import '../styles/sass/style.scss';
+import groq from 'groq';
+import client from '../client';
+
+const Food = (props) => {
+  const { header = '', image = '', alt = '', description = '' } = props;
+  return (
+    <div>
+      <h1>{header}</h1>
+      <img src={image} alt={alt}></img>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+const query = groq`*[_type == 'food'][0]{
+    header,
+    "image": image.asset->url,
+    alt,
+    description
+  }`;
+
+Food.getInitialProps = async function () {
+  return await client.fetch(query);
+};
+
+export default Food;
