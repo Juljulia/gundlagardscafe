3833a9eba5937f60f9690b70fe59253677a315ac
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-06 14:21:37 +0200

    variables

--- web/client.js
+++ web/client.js
@@ -1,7 +1,7 @@
-import sanityClient from "@sanity/client";
+import sanityClient from '@sanity/client';
 
 export default sanityClient({
-  projectId: process.env.SANITY_STUDIO_API_PROJECT_ID, // you can find this in sanity.json
-  dataset: process.env.SANITY_STUDIO_API_DATASET, // or the name you chose in step 1
+  projectId: 'mo66uxmx', // you can find this in sanity.json
+  dataset: 'production', // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
 });
