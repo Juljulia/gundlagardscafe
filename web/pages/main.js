cca5d6ac1a4e53e81e87bb2bfb356c398ed0d3f8
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-07 15:46:12 +0200

    presenting content

--- web/pages/main.js
+++ web/pages/main.js
@@ -1,27 +1,49 @@
 import groq from 'groq';
 import imageUrlBuilder from '@sanity/image-url';
+import Link from 'next/link';
 
 import client from '../client';
 import '../styles/sass/style.scss';
+import IconLink from '../components/Iconlink';
+import Hero from '../components/Hero';
 
 const builder = imageUrlBuilder(client);
 
-const Main = ({ header, imageGrid }) => {
+const Main = ({ header, heroImage, iconLink, imageGrid }) => {
   return (
     <div>
-      {header && <h1>{header}</h1>}
+      {heroImage && (
+        <Hero
+          alt={heroImage.alt}
+          image={builder.image(heroImage.asset).url()}
+          icon={builder.image(heroImage.icon).url()}
+        />
+      )}
+      {/* <div>
+        {iconLink &&
+          iconLink.map((icon, i) => (
+            <IconLink
+              key={i}
+              slug={icon.links.link}
+              icon={builder.image(icon.image.asset).url()}
+            />
+          ))}
+      </div>
+
       <div>
         {imageGrid &&
           imageGrid.map((image, i) => (
             <img key={i} src={builder.image(image.image.asset).url()}></img>
           ))}
-      </div>
+      </div> */}
     </div>
   );
 };
 
 const query = groq`*[_type == 'main'][0]{
     header,
+    heroImage,
+    iconLink,
     imageGrid
   }`;
 
