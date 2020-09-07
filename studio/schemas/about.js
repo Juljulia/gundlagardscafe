bcc24f70683173965f73db05ef904b05a15d70d7
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-07 13:39:30 +0200

    add schemas

--- studio/schemas/about.js
+++ studio/schemas/about.js
@@ -8,5 +8,27 @@ export default {
       name: 'header',
       type: 'string',
     },
+    {
+      name: 'heroImage',
+      type: 'heroImage',
+      title: 'Helbild',
+      description: 'Första helbilden på sidan',
+    },
+    {
+      title: 'Underrubrik',
+      name: 'subheading',
+      type: 'string',
+    },
+    {
+      title: 'Beskrivning',
+      name: 'description',
+      type: 'text',
+    },
+    {
+      name: 'imageGrid',
+      type: 'imageGrid',
+      title: 'Bilder till collage',
+      description: 'Välj fem bilder.',
+    },
   ],
 };
