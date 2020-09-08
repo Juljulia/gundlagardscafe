5735a16698b8f67a4fd549e03a68160cf1dcebc1
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-08 16:55:46 +0200

    hero

--- web/components/Hero.js
+++ web/components/Hero.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import colors from '../config/colors';
-
 const StyledHero = styled.div`
   height: 90vh;
   width: 100%;
   img {
-    height: 100%;
-    width: 100%;
     object-fit: cover;
-    border: 2px solid ${colors.primary};
   }
 `;
 
-const Hero = ({ image, icon, alt }) => (
-  <StyledHero>
-    <img src={image} alt={alt}></img>
-    <img src={icon} className="icon"></img>
-  </StyledHero>
-);
+const Hero = ({ image, icon, alt }) => {
+  return (
+    <StyledHero>
+      <img src={image} alt={alt}></img>
+      <div>
+        <img src={icon}></img>
+      </div>
+    </StyledHero>
+  );
+};
 
 export default Hero;
