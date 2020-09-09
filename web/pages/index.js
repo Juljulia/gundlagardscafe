bc03ee5b2d969a3f4b980a6767355345dd8c4d76
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-09 10:23:57 +0200

    adding instagramfeed

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,18 +1,19 @@
 import groq from 'groq';
-import Link from 'next/link';
 
 import client from '../sanity/client';
-import IconLink from '../components/Iconlink';
 import Hero from '../components/Hero';
+import IconLink from '../components/Iconlink';
 import Menu from '../components/Menu';
 import urlBuild from '../sanity/imageBuilder';
 
-const Index = ({ content }) => {
+const Index = ({ content, instagram }) => {
   const heroImage = urlBuild(content.heroImage.asset);
   const heroIcon = urlBuild(content.heroImage.icon);
   const iconLinks = content.iconLink;
   const imageGrid = content.imageGrid;
-
+  const instagramFeed =
+    instagram.graphql.user.edge_owner_to_timeline_media.edges;
+  const instaFour = instagramFeed.slice(0, 4);
   return (
     <section>
       <h1>{content.header}</h1>
@@ -36,6 +37,12 @@ const Index = ({ content }) => {
             <img key={i} src={urlBuild(image.image.asset)}></img>
           ))}
       </div>
+      <div>
+        {instaFour &&
+          instaFour.map((image, i) => (
+            <img key={i} src={image.node.display_url}></img>
+          ))}
+      </div>
     </section>
   );
 };
@@ -47,13 +54,18 @@ const query = groq`*[_type == 'main'][0]{
     imageGrid
   }`;
 
-export const getStaticProps = async () => {
+export async function getStaticProps() {
   const content = await client.fetch(query);
+
+  const res = await fetch('https://www.instagram.com/gundlagardscafe/?__a=1');
+  const json = await res.json();
+
   return {
     props: {
       content,
+      instagram: json,
     },
   };
-};
+}
 
 export default Index;
