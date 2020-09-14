6e33e80f062784c74086071a59bd15f2a06f1f7c
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-14 18:49:48 +0200

    changed dir name

--- web/sanity/imageBuilder.js
+++
@@ -1,7 +0,0 @@
-import client from './client';
-import imageUrlBuilder from '@sanity/image-url';
-
-const builder = imageUrlBuilder(client);
-const urlBuild = (source) => builder.image(source).url();
-
-export default urlBuild;
