c9d04f6a3d8bda15acec08a6325a74f69fe071d6
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-14 18:48:09 +0200

    changed dir name

--- web/components/ImageGrid.js
+++ web/components/ImageGrid.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import urlBuild from '../sanity/imageBuilder';
+import urlBuild from '../functions/imageBuilder';
 
 const StyledGrid = styled.div`
   .gridcontainter {
