185c53263c965f65947f6f75a5422b93bc2e10ab
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-15 10:58:02 +0200

    styling

--- web/components/StyledLink.js
+++ web/components/StyledLink.js
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 
-import colors from '../config/colors';
-
 const StyledLink = ({ as, children, className, href }) => (
   <Link href={href} as={as} passHref>
     <a className={className}>{children}</a>
@@ -10,26 +8,24 @@ const StyledLink = ({ as, children, className, href }) => (
 );
 
 export default styled(StyledLink)`
-  background-color: ${colors.primary};
-  color: ${colors.white};
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.white};
   text-decoration: none;
   transition: all 0.2s ease-in-out;
   box-shadow: 0px 4px 6px rgba(205, 133, 1, 0.25);
   border-radius: 11px;
   padding: 8px 40px;
 
+  font-family: Amatic SC;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 25px;
+
   &:hover {
-    text-decoration: underline;
     box-shadow: inset -1px -1px 6px rgba(135, 88, 1, 0.69);
   }
 
   &:active {
     box-shadow: inset 0px 4px 6px #b37401;
-  }
-  a {
-    font-family: Amatic SC;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 25px;
   }
 `;
