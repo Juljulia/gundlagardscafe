1152a04aa50edb1220aa909b275a7f4c5a3213f6
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-15 10:27:58 +0200

    add theme

---
+++ web/config/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: '#CD8501',
+    secondary: '#FF5D84',
+    red: '#C75516',
+    green: '#959967',
+    white: '#FFF',
+    black: '#2B2928',
+  },
+};
+
+export default theme;
