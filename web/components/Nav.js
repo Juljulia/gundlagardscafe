7b4dfcaef09c551e9d890996cf970e81360d2875
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-23 08:41:27 +0200

    add scrollToBottom

--- web/components/Nav.js
+++ web/components/Nav.js
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 import { Squash as Hamburger } from 'hamburger-react';
-import { animateScroll } from 'react-scroll';
 import PropTypes from 'prop-types';
 import 'lazysizes';
 
 const Nav = () => {
   const [isOpen, setOpen] = React.useState(false);
-
+  const scrollToBottom = () => {
+    window.scroll(0, document.body.scrollHeight);
+  };
   return (
     <Container show={isOpen}>
       <div className="burger__wrapper">
@@ -47,9 +48,7 @@ const Nav = () => {
           <a onClick={() => setOpen(false)}>Om oss</a>
         </Link>
 
-        <a onClick={() => animateScroll.scrollToBottom() + setOpen(false)}>
-          Kontakt
-        </a>
+        <a onClick={() => scrollToBottom() + setOpen(false)}>Kontakt</a>
 
         <Link href="/fragor-svar">
           <a onClick={() => setOpen(false)}>Frågor och svar</a>
@@ -102,9 +101,7 @@ const Nav = () => {
             <a onClick={() => setOpen(false)}>Om oss</a>
           </Link>
 
-          <a onClick={() => animateScroll.scrollToBottom() + setOpen(false)}>
-            Kontakt
-          </a>
+          <a onClick={() => scrollToBottom() + setOpen(false)}>Kontakt</a>
 
           <Link href="/fragor-svar">
             <a onClick={() => setOpen(false)}>Frågor och svar</a>
