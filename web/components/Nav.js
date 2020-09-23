33d5d5ffdca1d783d993f8cbe8815c65a126b1b5
Julia Karlsson <karlssonjulia.m@gmail.com>
2020-09-23 11:49:46 +0200

    add taget blank

--- web/components/Nav.js
+++ web/components/Nav.js
@@ -78,7 +78,9 @@ const Nav = () => {
           </div>
         </div>
         <Link href="https://www.google.com/maps/place/Gundla+G%C3%A5rdscaf%C3%A9/@57.6862902,12.0260549,17z/data=!3m1!4b1!4m5!3m4!1s0x464ff3a5d0157c63:0xdd291fa6017dc04a!8m2!3d57.6862902!4d12.0282436">
-          <a className="mail-to">Adress: Gundla mosse 32, 412 76 Göteborg</a>
+          <a className="mail-to" target="_blank">
+            Adress: Gundla mosse 32, 412 76 Göteborg
+          </a>
         </Link>
         <div className="tent">
           <img data-src="/tent-menu.png" className="lazyload"></img>
@@ -132,7 +134,9 @@ const Nav = () => {
             <p>mobil: 0708-84 07 17 </p>
           </div>
           <Link href="https://www.google.com/maps/place/Gundla+G%C3%A5rdscaf%C3%A9/@57.6862902,12.0260549,17z/data=!3m1!4b1!4m5!3m4!1s0x464ff3a5d0157c63:0xdd291fa6017dc04a!8m2!3d57.6862902!4d12.0282436">
-            <a className="mail-to">Adress: Gundla mosse 32, 412 76 Göteborg</a>
+            <a className="mail-to" target="_blank">
+              Adress: Gundla mosse 32, 412 76 Göteborg
+            </a>
           </Link>
         </div>
 
